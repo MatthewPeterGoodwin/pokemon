f4481b61069a95ba4b336148e1b774bc1325f065
Matt Goodwin <matthewppgoodwin@sky.com>
2024-10-14 16:09:02 +0100

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+async function getData() {
+  const res = await fetch('https://pokeapi.co/api/v2/pokemon/blastoise')
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+
+  return res.json()
+}
+
+export default async function Index() {
+  const data = await getData()
+  console.log(data)
+  return (
+    <div>
+      <p>{data.name}</p>
+    </div>
+  )
+};
