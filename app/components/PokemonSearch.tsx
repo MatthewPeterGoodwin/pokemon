443e4a4c047c82990bc32bda62dcf20059ab7f8d
MatthewPeterGoodwin <135028209+mattpgoodwinubds@users.noreply.github.com>
2024-10-22 11:51:45 +0100

    Added tailwindcss to add borders and centre

--- app/components/PokemonSearch.tsx
+++ app/components/PokemonSearch.tsx
@@ -34,29 +34,33 @@ export default function PokemonSearch() {
     }
 
     return (
-        <div className="text-2xl content-center	">
-            <form onSubmit={handleSubmit}>
-                <label>
-                    <input
-                        name="myInput"
-                        value={pokemonName}
-                        onChange={handleInputChange}
-                        placeholder="Enter Pokémon name"
-                    />
-                </label>
-                <button type="submit">Search</button>
-            </form>
-            {error && <p style={{ color: 'red' }}>{error}</p>}
-            {pokemonData && (
-                <div className="text-2xl">
-                    <img src={pokemonData.sprites.front_default} alt={pokemonData.name} />
-                    <h1>
-                        {pokemonData.types.map((t: PokemonType) => {
-                            return t.type.name + " "
-                        })}
-                    </h1>
-                </div>
-            )}
-        </div>
+        <div className="text-2xl flex flex-col items-center justify-center min-h-screen">
+        <form onSubmit={handleSubmit} className="mb-4">
+            <label>
+                <input
+                    name="myInput"
+                    value={pokemonName}
+                    onChange={handleInputChange}
+                    placeholder="Enter Pokémon name"
+                    className="border border-gray-300 rounded p-2"
+                />
+            </label>
+            <button type="submit" disabled={!pokemonName} className="ml-2 px-4 py-2 bg-blue-500 text-white rounded disabled:opacity-50">
+                Search
+            </button>
+        </form>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
+        {pokemonData && (
+            <div className="text-2xl border-solid border-2 border-sky-500 p-4 flex flex-col items-center">
+                <img src={pokemonData.sprites.front_default} alt={pokemonData.name} className="mb-4" />
+                <h1 className="text-center">
+                    {pokemonData.types.map((t: PokemonType) => {
+                        return t.type.name + " ";
+                    })}
+                </h1>
+            </div>
+        )}
+    </div>
+    
     )
 }
